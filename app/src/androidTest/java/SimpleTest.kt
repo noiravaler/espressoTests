27164a78a0ec6b2f3f578c4d4fcf18ad3aee6680
ext_tmilkina <ext_tmilkina@rencredit.ru>
2020-11-02 14:41:22 +0300

    junit5

---
+++ app/src/androidTest/java/SimpleTest.kt
@@ -0,0 +1,30 @@
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import co.`fun`.testgiphy.MainActivity
+import co.`fun`.testgiphy.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class SimpleTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun listGoesOverTheFoldTest() {
+        onView(withId(R.id.menu_trending_search)).perform(click())
+        Assertions.assertEquals(42, Integer.sum(19, 23))
+    }
+
+    @Test
+    fun listGoesOverTheFold2Test() {
+        onView(withId(R.id.menu_trending_search)).perform(click())
+    }
+}
