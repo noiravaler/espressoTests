f60e68ff9d1d1129d4d51b62e77e9da2ebc85574
ext_tmilkina <ext_tmilkina@rencredit.ru>
2020-11-18 16:30:50 +0300

    fix which work

--- app/src/androidTest/java/SimpleTest.kt
+++ app/src/androidTest/java/SimpleTest.kt
@@ -1,21 +1,20 @@
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.rule.ActivityTestRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import co.`fun`.testgiphy.MainActivity
 import co.`fun`.testgiphy.R
 import org.junit.Rule
 import org.junit.Test
 import org.junit.jupiter.api.Assertions
-import org.junit.runner.RunWith
 
 //@RunWith(AndroidJUnit4ClassRunner::class)
 class SimpleTest {
     @Rule
     @JvmField
-    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+    var activityActivityTestRule = ActivityScenarioRule(
+        MainActivity::class.java
+    )
 
     @Test
     fun listGoesOverTheFoldTest() {
